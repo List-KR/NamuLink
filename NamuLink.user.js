e99e9d82370fe9eb440d840340e52faa39c9e239
PiQuark6046 <piquark6046@proton.me>
2022-07-24 16:05:59 +0000

    Publish 1.0

--- NamuLink.user.js
+++ NamuLink.user.js
@@ -8,11 +8,10 @@
 // @downloadURL  https://github.com/List-KR/NamuLink/raw/main/NamuLink.user.js
 // @license      MIT
 //
-// @version      1.0d1
+// @version      1.0
 // @author       PiQuark6046 and contributors
 //
 // @match        https://namu.wiki/w/*
-// @exclude      https://namu.wiki/w/%EB%82%98%EB%AC%B4%EC%9C%84%ED%82%A4:%EB%8C%80%EB%AC%B8*
 // @exclude      https://namu.wiki/w/%EC%9C%84%ED%82%A4%EC%9A%B4%EC%98%81:*
 //
 // @description        NamuLink blocks the license-abused PowerLink advertisement on NamuWiki.
@@ -21,12 +20,12 @@
 // @run-at       document-start
 // ==/UserScript==
 
-(function() {
+(() => {
     'use strict'
 
-    const unsafeWindow = window || unsafeWindow
+    const win = unsafeWindow != undefined ? unsafeWindow : window
 
-    const Generation =
+    const Gen =
     {
         Parents: (element) =>
         {
@@ -39,12 +38,7 @@
         },
         Children: (element) =>
         {
-            var data = [element]
-            while (Array.from(data).every((element) => { element.children.length == 0 }))
-            {
-                
-            }
-            return data.filter((FilterElement) => { return FilterElement != element })
+            return Array.from(element.querySelectorAll("*"))
         },
         Peers: (element) =>
         {
@@ -52,31 +46,49 @@
         }
     }
 
-    const CSSMatch = 
+    const CheckPowerLinkLabel = (thisArg, argsList) =>
     {
-        Visible: (array) =>
+        return argsList[0] == "click" && (4.38 < thisArg.offsetWidth / thisArg.offsetHeight < 4.39)
+    }
+
+    const HideElement = setInterval((e) =>
+    {
+        e.style.display = "none"
+    }, 10)
+
+    EventTarget.prototype.addEventListener = new Proxy(
+        EventTarget.prototype.addEventListener,
         {
-            return Array.from(array).filter((element) => 
+            apply: (target, thisArg, argsList) =>
             {
-                return element.offsetHeight >= 10 && element.offsetWidth >= 10
-            })
+                if (CheckPowerLinkLabel)
+                {
+                    HideElement(Gen.Parents(thisArg).filter((e) => e.offsetWidth / e.offsetHeight > 1)[0])
+                    Reflect.apply(target, thisArg, argsList)
+                }
+                else
+                {
+                    Reflect.apply(target, thisArg, argsList)
+                }
+            }
         }
-    }
-    const Format =
-    {
-        Domain: (text) =>
+    )
+
+    TextDecoder.prototype.decode = new Proxy(
+        TextDecoder.prototype.decode,
         {
-            return new unsafeWindow.RegExp(/[\u3131-\uD79DA-z0-9]{1,}[^\u3131-\uD79DA-z0-9]{0,}(com|net|org|kr|한국|[\u3131-\uD79DA-z0-9]{2}[^\u3131-\uD79DA-z0-9]{0,}kr|[\u3131-\uD79DA-z0-9]{2}[^\u3131-\uD79DA-z0-9]{0,}한국|me|at|io|club|online|market|store|site|one|space|blog|tech|llc|homes|press|software|co|cloud|live|[A-z]{2,8})/gui).test(text)
-        },
-        HEXColor: (text) =>
-        {         
-            return new unsafeWindow.RegExp(/rgb\([0-9]{1,3},[ ]{0,}[0-9]{1,3},[ ]{0,}[0-9]{1,3}\)/).test(text)
+            apply: (target, thisArg, argsList) =>
+            {
+                const original = Reflect.apply(target, thisArg, argsList)
+                if (/\/\/adcr\.naver\.com\//.test(original.toString()))
+                {
+                    return new RangeError()
+                }
+                else
+                {
+                    return original
+                }
+            }
         }
-    }
-    
-    unsafeWindow.console.log("NamuLink: Initialized.")
-
-    var VisibleElementArray = CSSMatch.Visible(document.querySelectorAll("*"))
-    new MutationObserver()
-
+    )
 })()
