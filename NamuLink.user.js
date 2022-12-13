a48b7f7475eaf5eb4a9fcf2dd635908ff99be79f
green1052 <34261355+green1052@users.noreply.github.com>
2022-12-13 23:34:58 +0900

    version up

--- NamuLink.user.js
+++ NamuLink.user.js
@@ -8,7 +8,7 @@
 // @downloadURL  https://cdn.jsdelivr.net/gh/List-KR/NamuLink@main/NamuLink.user.js
 // @license      MIT
 //
-// @version      1.6.3
+// @version      2.0.0
 // @author       PiQuark6046 and contributors
 //
 // @match        https://namu.wiki/*
