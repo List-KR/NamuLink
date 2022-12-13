9fd474b2f3bcc91fc42077f98574f6f9e6316a35
green1052 <34261355+green1052@users.noreply.github.com>
2022-12-14 01:22:58 +0900

    :((((((((

--- NamuLink.user.js
+++ NamuLink.user.js
@@ -20,172 +20,158 @@
 // ==/UserScript==
 
 (() => {
-	'use strict'
+    "use strict";
 
-	/// APIs
+    /// APIs
 
-	const Gen =
-	{
-		Parents: (element) =>
-		{
-			let data = [element]
-			while (data[0].parentElement !== null)
-			{
-				data = [data[0].parentElement].concat(data)
-			}
-			return data.filter((FilterElement) => { return FilterElement !== element })
-		},
-		Children: (element) =>
-		{
-			return Array.from(element.querySelectorAll("*"))
-		},
-		Peers: (element) =>
-		{
-			return Array.from(element.parentElement.children).filter((FElement) => { return FElement !== element })
-		}
-	}
+    const Gen = {
+        Parents: (element) => {
+            const data = [];
+            let current = element;
 
-	const GetBoxRate = (e) =>
-	{
-		return e.offsetWidth / e.offsetHeight
-	}
+            while ((current = current.parentElement) !== null) {
+                data.push(current);
+            }
 
-	const HideElementsImportant = (e) =>
-	{
-		const target = e.filter((k) => k !== undefined)
-		if (target.length === 0) return 0
-		setInterval((k) =>
-		{
-			Array.from(k).forEach((o) => { o.style.setProperty("display", "none", "important") })
-		}, 50, target)
-		console.debug("NamuLink: HideElementsImportant: ", target)
-		return target.length
-	}
+            return data;
+        },
+        Children: (element) => {
+            return Array.from(element.querySelectorAll("*"));
+        },
+        // Peers: (element) => {
+        //     return Array.from(element.parentElement.children).filter((FElement) => {
+        //         return FElement !== element
+        //     });
+        // }
+    }
 
-	const HideArcaliveAdver = () =>
-	{
-		Array.from(document.querySelectorAll("iframe[src]")).filter((e) => { return /\/\/arca\.live\/external\/callad/.test(e.getAttribute("src")) })
-		.forEach((e) => { HideElementsImportant(Gen.Parents(e).filter((o) => { return o.innerText === "" && getComputedStyle(o).getPropertyValue("padding-bottom").replace(/px$/, "") > 15 }))})
-	}
+    const GetBoxRate = (e) => {
+        return e.offsetWidth / e.offsetHeight;
+    }
 
-	const HideJSONPowerLink = () =>
-	{
-		HideElementsImportant(Array.from(document.querySelectorAll("iframe[src*='//arca.live/static/ad/powerlink.html?size=']")).filter((e) => { return e.offsetHeight > 100 && e.offsetWidth > 100 }))
-	}
+    const HideElementsImportant = (e) => {
+        if (e.length === 0) return 0;
 
-	const GetPendingPowerLink = () =>
-	{
-		return Array.from(document.querySelectorAll("*"))
-			.filter((e) => { return /^(|[​\n\t ]{1,})$/.test(e.innerText) && getComputedStyle(e).getPropertyValue("margin-top").replace(/px$/, "") > 20 // zero-width space (U+200B) included
-			&& Array.from(document.querySelectorAll("*"))
-			.filter((k) => { return getComputedStyle(k).getPropertyValue("animation-iteration-count") === "infinite" })
-			.every((k) => { return e.contains(k) }) })
-	}
+        for (const element of e) {
+            element.style.setProperty("display", "none", "important");
+        }
 
-	/// Main
-	// Convert to string: String.fromCharCode TextDecoder.prototype.decode String.prototype.normalize String.fromCodePoint
-	// 
+        console.debug("NamuLink: HideElementsImportant: ", e);
+        return e.length;
+    }
 
-	let PowerLinkLabelCache = []
-	const BitArrayObjs8 = [Uint8ClampedArray, Int8Array, Uint8Array]
+    const HideArcaliveAdver = () => {
+        for (const element of document.querySelectorAll(`iframe[src*="//arca.live/external/callad"]`)) {
+            HideElementsImportant(Gen.Parents(element).filter((o) => {
+                return o.innerText === "" && getComputedStyle(o).getPropertyValue("padding-bottom").replace(/px$/, "") > 15
+            }))
+        }
+    }
 
-	EventTarget.prototype.addEventListener = new Proxy(
-		EventTarget.prototype.addEventListener,
-		{
-			apply: (target, thisArg, argsList) =>
-			{
-				if (/^https:\/\/namu\.wiki\/w\//.test(location.href) && argsList[0] === "click" && GetBoxRate(thisArg) > 2) // PowerLinkLabelCache Label
-				{
-					PowerLinkLabelCache.push(thisArg)
-				}
-				else if (argsList[0] === "click" && /^.{1,}$/.test(thisArg.innerText)) // PowerLinkLabelCache Content
-				{
-					for (let o of PowerLinkLabelCache)
-					{
-						if (HideElementsImportant(Gen.Parents(o).filter((e) => { return GetBoxRate(e) > 1 && getComputedStyle(e).getPropertyValue("margin-top").replace(/px$/, "") > 20 })
-						.filter((e) => { return e.innerText === "" && Gen.Children(e).includes(o) }))
-						> 0)
-						{
-							console.debug("NamuLink: EventTarget.prototype.addEventListener handler: ", PowerLinkLabelCache)
-							PowerLinkLabelCache = []
-							break
-						}
-					}
-				}
-				Reflect.apply(target, thisArg, argsList)
-			}
-		}
-	)
+    const HideJSONPowerLink = () => {
+        HideElementsImportant(Array.from(document.querySelectorAll("iframe[src*='//arca.live/static/ad/powerlink.html?size=']")).filter((e) => {
+            return e.offsetHeight > 100 && e.offsetWidth > 100;
+        }));
+    }
 
-	for (let Obj of BitArrayObjs8)
-	{
-		Obj.prototype.slice = new Proxy(
-			Obj.prototype.slice,
-			{
-				apply: (target, thisArg, argsList) =>
-				{
-					const Original = Reflect.apply(target, thisArg, argsList)
-					if (new TextDecoder().decode(Original) instanceof ReferenceError
-					|| new TextDecoder().decode(Obj.of(Original).reverse()) instanceof ReferenceError)
-					{
-						console.debug("NamuLink: " + Obj.name + ".prototype.slice handler: ", Original)
-						HideElementsImportant(GetPendingPowerLink())
-						return crypto.getRandomValues(new Obj(Original.byteLength))
-					}
-					else
-					{
-						return Original
-					}
-				}
-			}
-		)
-	}
+    const GetPendingPowerLink = () => {
+        return Array.from(document.querySelectorAll("*"))
+            .filter((e) => {
+                return /^(|[​\n\t ]{1,})$/.test(e.innerText) &&
+                    getComputedStyle(e).getPropertyValue("margin-top").replace(/px$/, "") > 20 && // zero-width space (U+200B) included
+                    Array.from(e.children).filter((e) => {
+                        return Array.from(e.querySelectorAll("*")).filter((e2) => {
+                            return getComputedStyle(e2).getPropertyValue("animation-iteration-count") === "infinite";
+                        });
+                    }).length > 0
+            });
+    };
 
-	for (let Obj of BitArrayObjs8)
-	{
-		Obj.from = new Proxy(
-			Obj.from,
-			{
-				apply: (target, thisArg, argsList) =>
-				{
-					const Original = Reflect.apply(target, thisArg, argsList)
-					if (new TextDecoder().decode(Original) instanceof ReferenceError
-					|| new TextDecoder().decode(Obj.of(Original).reverse()) instanceof ReferenceError)
-					{
-						console.debug("NamuLink: " + Obj.name + ".from handler: ", Original)
-						HideElementsImportant(GetPendingPowerLink())
-						return crypto.getRandomValues(new Obj(Original.byteLength))
-					}
-					else
-					{
-						return Original
-					}
-				}
-			}
-		)
-	}
+    /// Main
+    // Convert to string: String.fromCharCode TextDecoder.prototype.decode String.prototype.normalize String.fromCodePoint
+    //
 
-	TextDecoder.prototype.decode = new Proxy(
-		TextDecoder.prototype.decode,
-		{
-			apply: (target, thisArg, argsList) =>
-			{
-				const Original = Reflect.apply(target, thisArg, argsList)
-				if (/\[+.+\/\/adcr\.naver\.com\/adcr\?.+,.+/.test(Original.toString()))
-				{
-					return new ReferenceError()
-				}
-				else
-				{
-					return Original
-				}
-			}
-		}
-	)
+    let PowerLinkLabelCache = [];
+    const BitArrayObjs8 = [Uint8ClampedArray, Int8Array, Uint8Array];
 
-	document.addEventListener("DOMContentLoaded", () =>
-	{
-		HideArcaliveAdver()
-		HideJSONPowerLink()
-	})
-})()
+    EventTarget.prototype.addEventListener = new Proxy(
+        EventTarget.prototype.addEventListener,
+        {
+            apply: (target, thisArg, argsList) => {
+                if (/^\/w\//.test(location.pathname) && argsList[0] === "click" && GetBoxRate(thisArg) > 2) {
+                    // PowerLinkLabelCache Label
+                    PowerLinkLabelCache.push(thisArg);
+                } else if (argsList[0] === "click" && /^.{1,}$/.test(thisArg.innerText)) {
+                    // PowerLinkLabelCache Content
+                    for (const o of PowerLinkLabelCache) {
+                        if (HideElementsImportant(Gen.Parents(o).filter((e) => {
+                                return GetBoxRate(e) > 1 && getComputedStyle(e).getPropertyValue("margin-top").replace(/px$/, "") > 20;
+                            })
+                                .filter((e) => {
+                                    return e.innerText === "" && Gen.Children(e).includes(o);
+                                }))
+                            > 0) {
+                            console.debug("NamuLink: EventTarget.prototype.addEventListener handler: ", PowerLinkLabelCache);
+                            PowerLinkLabelCache = [];
+                            break;
+                        }
+                    }
+                }
+
+                Reflect.apply(target, thisArg, argsList);
+            }
+        }
+    )
+
+    for (let Obj of BitArrayObjs8) {
+        Obj.prototype.slice = new Proxy(
+            Obj.prototype.slice,
+            {
+                apply: (target, thisArg, argsList) => {
+                    const Original = Reflect.apply(target, thisArg, argsList);
+
+                    if (new TextDecoder().decode(Original) instanceof ReferenceError
+                        || new TextDecoder().decode(Obj.of(Original).reverse()) instanceof ReferenceError) {
+                        console.debug("NamuLink: " + Obj.name + ".prototype.slice handler: ", Original);
+                        HideElementsImportant(GetPendingPowerLink());
+                        return crypto.getRandomValues(new Obj(Original.byteLength));
+                    } else {
+                        return Original;
+                    }
+                }
+            }
+        )
+
+        Obj.from = new Proxy(
+            Obj.from,
+            {
+                apply: (target, thisArg, argsList) => {
+                    const Original = Reflect.apply(target, thisArg, argsList);
+                    if (new TextDecoder().decode(Original) instanceof ReferenceError
+                        || new TextDecoder().decode(Obj.of(Original).reverse()) instanceof ReferenceError) {
+                        console.debug("NamuLink: " + Obj.name + ".from handler: ", Original);
+                        HideElementsImportant(GetPendingPowerLink());
+                        return crypto.getRandomValues(new Obj(Original.byteLength));
+                    } else {
+                        return Original;
+                    }
+                }
+            }
+        )
+    }
+
+    TextDecoder.prototype.decode = new Proxy(
+        TextDecoder.prototype.decode,
+        {
+            apply: (target, thisArg, argsList) => {
+                const Original = Reflect.apply(target, thisArg, argsList);
+                return /\[+.+\/\/adcr\.naver\.com\/adcr\?.+,.+/.test(Original) ? new ReferenceError() : Original;
+            }
+        }
+    )
+
+    document.addEventListener("DOMContentLoaded", () => {
+        HideArcaliveAdver();
+        HideJSONPowerLink();
+    })
+})();
