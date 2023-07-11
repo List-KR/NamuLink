35b35054911c809421120c29bbe9b34dbce3febe
PiQuark6046 <piquark6046@proton.me>
2023-07-11 08:06:34 +0000

    Add basic API of NamuLink

--- NamuLink.user.ts
+++ NamuLink.user.ts
@@ -25,5 +25,47 @@ type unsafeWindow = typeof window
 declare const unsafeWindow: unsafeWindow
 
 (function () {
-  
+  const win = typeof unsafeWindow !== "undefined" ? unsafeWindow : window
+  /**
+   * Leave debug message to console.
+   * @param message The message to leave.
+   */
+  function NamuLinkDebug(message: any): void {
+    console.debug(`NamuLink: ${new Error().stack.split('\n')[1].trim()}: ${message}`)
+  }
+
+  /**
+   * Hide elements by setting their display to none.
+   * @param elements The elements to hide.
+   * @returns The count of elements that were hidden.
+   */
+  function HideElementsImportant(elements: Array<HTMLElement>): number {
+    if (elements.length === 0) return 0
+    for (let element of elements) {
+      element.style.setProperty('display', 'none', 'important')
+    }
+    NamuLinkDebug(elements)
+    return elements.length
+  }
+
+  /**
+   * Get parents of a element.
+   * @param element The element to get parents of.
+   */
+  function Parents (element: HTMLElement) {
+    const ParentsArray: Array<HTMLElement> = []
+    let CurrentElement: HTMLElement = element
+    while ((CurrentElement = CurrentElement.parentElement) !== null) {
+      ParentsArray.push(CurrentElement)
+    }
+    return ParentsArray
+  }
+
+  /**
+   * Get children of a element.
+   * @param element The element to get children of.
+   */
+  function Children (element: HTMLElement) {
+    return Array.from(element.querySelectorAll('*'))
+  }
 })();
