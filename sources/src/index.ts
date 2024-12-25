41d6d61c7e97629d93465244e8d0ab6689c71cd9
piquark6046 <j.w.so@adguard.com>
2024-12-25 02:40:57 +0000

    feat: mock Proxy construct

--- sources/src/index.ts
+++ sources/src/index.ts
@@ -1,96 +1,25 @@
-import IsValidDomain from 'is-valid-domain'
-import PLimit from 'p-limit'
-import {SplitElementsIntoSubArrayLength} from 'multithread-array'
-
 type unsafeWindow = typeof window
 // eslint-disable-next-line @typescript-eslint/naming-convention
 declare const unsafeWindow: unsafeWindow
 
 const Win = typeof unsafeWindow !== 'undefined' ? unsafeWindow : window
 
-const RemoveElements = (TargetElements: HTMLElement[]) => {
-	TargetElements.forEach(TargetElement => {
-		TargetElement.setAttribute('style', 'visibility: hidden !important; width: 1px !important; height: 1px !important;')
-	})
-}
-
-const RemovePowerLinkAdWorker = (SearchedElements: HTMLElement[]): HTMLElement[] => {
-	let TargetedElements: HTMLElement[] = []
-	TargetedElements = SearchedElements.filter(TargetedElement => {
-		return Number(getComputedStyle(TargetedElement).getPropertyValue('margin-top').replace('px', '')) > 10
-		|| (Array.from(TargetedElement.children).some(Children => {
-			return Number(getComputedStyle(Children).getPropertyValue('padding-left').replace('px', '')) > 5
-			&& Number(getComputedStyle(Children).getPropertyValue('padding-top').replace('px', '')) > 5
-			&& Number(getComputedStyle(Children).getPropertyValue('padding-right').replace('px', '')) > 5
-			&& Number(getComputedStyle(Children).getPropertyValue('padding-bottom').replace('px', '')) > 5
-		}))
-	})
-	TargetedElements = TargetedElements.filter(SearchedElement => {
-		return Array.from(SearchedElement.querySelectorAll('a,span,div')).filter(HTMLInElement => {
-			return HTMLInElement instanceof HTMLElement
-			&& IsValidDomain((HTMLInElement.innerText.replaceAll(/​ /g, '').match(/^[^/]+(?=\/)?/g) ?? [''])[0], { allowUnicode: true, subdomain: true }) // Zero width space and space should be removed.
-		}).length >= 2
-		|| Array.from(SearchedElement.querySelectorAll('*')).filter(HTMLInElement => {
-			return HTMLInElement instanceof HTMLElement && getComputedStyle(HTMLInElement).getPropertyValue('animation-duration') === '1.5s'
-		}).length >= 4
-	})
-	TargetedElements = TargetedElements.filter(SearchedElement => {
-		return Array.from(SearchedElement.querySelectorAll('*[href^="/RecentChanges"]')).filter(HTMLInElement => {
-			return Number(getComputedStyle(HTMLInElement).getPropertyValue('width').replaceAll('px', '')) > 50
-			&& Number(getComputedStyle(HTMLInElement).getPropertyValue('height').replaceAll('px', '')) > 10
-		}).length === 0
-	})
-	TargetedElements = TargetedElements.filter(SearchedElement => {
-		return Array.from(SearchedElement.querySelectorAll('*[href^="#fn-"]')).filter(HTMLInElement => {
-			return HTMLInElement instanceof HTMLElement && /\[[0-9]+\]/.test(HTMLInElement.innerText.replaceAll(/​ /g, '')) // Zero width space and space should be removed.
-		}).length === 0
-	})
-	TargetedElements = TargetedElements.filter(SearchedElement => {
-		return Array.from(SearchedElement.querySelectorAll('*[href]')).filter(HTMLInElement => {
-			return getComputedStyle(HTMLInElement, 'before').getPropertyValue('font-family') === 'Ionicons'
-		}).length === 0
-	})
-	TargetedElements = TargetedElements.filter(SearchedElement => {
-		return Array.from(SearchedElement.querySelectorAll('*')).every(HTMLInElement => 
-			!(/목차/.test(getComputedStyle(HTMLInElement, 'before').getPropertyValue('content'))
-			|| /목차/.test(getComputedStyle(HTMLInElement, '::before').getPropertyValue('content')))
-		)
-	})
-	TargetedElements = TargetedElements.filter(SearchedElement => {
-		return Array.from(SearchedElement.querySelectorAll('img[alt^="파일:"]')).length < 1
-	})
-	TargetedElements = TargetedElements.filter(SearchedElement => {
-		return !SearchedElement.textContent.includes('https://board.namu.wiki/b/')
-	})
-	TargetedElements = TargetedElements.filter(SearchedElement => {
-		return !SearchedElement.textContent.includes('이 사용자는 특수 권한을 가지고 있습니다')
-	})
-	TargetedElements = TargetedElements.filter(SearchedElement => {
-		return Array.from(SearchedElement.querySelectorAll('a[href="/RecentChanges"]')).length < 1
-	})
-	TargetedElements = TargetedElements.filter(SearchedElement => {
-		return Array.from(SearchedElement.querySelectorAll('a[href="#"][class]')).length < 1
-	})
-	return TargetedElements
-}
-
-const RemovePowerLinkAd = async () => {
-	const ElementsInArticle = []
-	ElementsInArticle.push(...Array.from(Win.document.querySelectorAll('div[class] div[class*=" "]:has(span ~ ul li) ~ div div[class] > div[class] div[class] ~ *[class]')))
-	ElementsInArticle.push(...Array.from(Win.document.querySelectorAll('div:not([class*=" "]) div[class] *[class]:not(:has(svg[viewBox="0 0 20 20"]))')))
-	ElementsInArticle.push(...Array.from(Win.document.querySelectorAll('div[class*=" "] > div[class*=" "]:has(span ~ ul li) ~ div[class*=" "] > div[class]')))
-	ElementsInArticle.push(...Array.from(Win.document.querySelectorAll('div[onmouseover][onmouseout] ~ div[class*=" "] > div[style]')))
-	let TargetedElements: HTMLElement[] = []
-	const PLimitInstance = PLimit((navigator.hardwareConcurrency ?? 4) < 4 ? 4 : navigator.hardwareConcurrency)
-	const PLimitJobs: Promise<HTMLElement[]>[] = []
-	for (const ElementsInArticleChunk of SplitElementsIntoSubArrayLength(ElementsInArticle, {Count: 2})) {
-		PLimitJobs.push(PLimitInstance(() => RemovePowerLinkAdWorker(ElementsInArticleChunk)))
+Win.Proxy = new Proxy(Win.Proxy, {
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
+	construct<T extends object>(Target: ProxyConstructor, Args: [T, ProxyHandler<T>], NewTarget: Function): object {
+		if (typeof Args[0] === 'object' && Array.isArray(Args[0]) && typeof Args[0][0] === 'object' && Array.isArray(Args[0][0])
+		&& typeof Args[0][0][1] === 'string' && /^#x=[A-Za-z0-9-+]+\/\/\/.+=?$/.test(Args[0][0][1])) {
+			setTimeout(() => {
+				Array.from(document.querySelectorAll('div[class*=" "] div[class]')).filter((TargetEle: HTMLElement) => {
+					return TargetEle.innerText === '' && Array.from(TargetEle.querySelectorAll('*')).filter((ChildEle: HTMLElement) => {
+						return getComputedStyle(ChildEle).getPropertyValue('animation').includes('infinite')
+					}).length >= 6
+				}).forEach((TargetEle: HTMLElement) => {
+					TargetEle.setAttribute('style', 'visibility: hidden !important; width: 1px !important; height: 1px !important;')
+				})
+			}, 2000)
+			return
+		}
+		return Reflect.construct(Target, Args, NewTarget)
 	}
-	TargetedElements = await Promise.all(PLimitJobs).then(PLimitResults => PLimitResults.flat())
-	console.debug('[NamuLink:index]: RemovePowerLinkAd:', TargetedElements)
-	RemoveElements(TargetedElements)
-}
-
-if (location.hostname !== 'board.namu.wiki' && !location.pathname.startsWith('/RecentDiscuss')) {
-	setInterval(RemovePowerLinkAd, 1500)
-}
+})
