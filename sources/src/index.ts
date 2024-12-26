d43f9834ed634ae2b669f768988ed2bf2f06fb18
piquark6046 <j.w.so@adguard.com>
2024-12-26 06:05:11 +0000

    fix: limit height CSS property

--- sources/src/index.ts
+++ sources/src/index.ts
@@ -8,7 +8,8 @@ let HideLeftover = () => {
 	setInterval(() => {
 		Array.from(document.querySelectorAll('div[class*=" "] div[class]')).filter((TargetEle: HTMLElement) => {
 			return (TargetEle.innerText === '' || TargetEle.innerText.includes('파워링크')) &&
-			(Number(getComputedStyle(TargetEle).getPropertyValue('margin-top').replace('px', '')) > 10 || Number(getComputedStyle(TargetEle).getPropertyValue('margin-bottom').replace('px', '')) > 10)
+			(Number(getComputedStyle(TargetEle).getPropertyValue('margin-top').replace('px', '')) > 10 || Number(getComputedStyle(TargetEle).getPropertyValue('margin-bottom').replace('px', '')) > 10) &&
+			Number(getComputedStyle(TargetEle).getPropertyValue('height').replace('px', '')) < 250
 		}).forEach((TargetEle: HTMLElement) => {
 			TargetEle.setAttribute('style', 'visibility: hidden !important; width: 1px !important; height: 1px !important;')
 		})
