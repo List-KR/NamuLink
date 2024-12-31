5b29d6622c9c5bb31c02110303abe1cf3e09b064
piquark6046 <j.w.so@adguard.com>
2024-12-31 08:41:02 +0000

    fix: powerlink mobile variant

--- sources/src/index.ts
+++ sources/src/index.ts
@@ -8,10 +8,10 @@ setInterval(() => {
   Array.from(document.querySelectorAll('div[class*=" "] div[class]')).filter(Filtered => Filtered instanceof HTMLElement &&
     (Filtered.innerText.includes('파워링크') || Filtered.innerText === '') && Number(getComputedStyle(Filtered).getPropertyValue('padding-top').replaceAll('px', '')) >= 8 &&
     Number(getComputedStyle(Filtered).getPropertyValue('min-height').replaceAll('px', '')) > 100 &&
-    Number(getComputedStyle(Filtered).getPropertyValue('height').replaceAll('px', '')) < 350
+    Number(getComputedStyle(Filtered).getPropertyValue('height').replaceAll('px', '')) < 400
   ).forEach(Target => Target.setAttribute('style', 'display: none !important;'))
   Array.from(document.querySelectorAll('div[class*=" "] div[class][style="display: none !important;"]')).filter(Filtered => Filtered instanceof HTMLElement &&
     Number(getComputedStyle(Filtered.parentElement).getPropertyValue('min-height').replaceAll('px', '')) > 100 &&
-    Number(getComputedStyle(Filtered.parentElement).getPropertyValue('height').replaceAll('px', '')) < 350
+    Number(getComputedStyle(Filtered.parentElement).getPropertyValue('height').replaceAll('px', '')) < 400
   ).forEach(Target => Target.parentElement.parentElement.remove())
 }, 2500)
