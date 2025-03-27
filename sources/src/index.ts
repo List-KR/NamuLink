0fe4db955e34adc961149af6323e1191b2af957f
piquark6046 <j.w.so@adguard.com>
2025-03-27 09:50:33 +0000

    fix: mobile leftover

--- sources/src/index.ts
+++ sources/src/index.ts
@@ -18,11 +18,8 @@ setInterval(() => {
   Array.from(document.querySelectorAll('div[class*=" "] div[class]')).filter(Filtered => Filtered instanceof HTMLElement &&
     (Filtered.innerText.includes('파워링크') || Filtered.innerText.replaceAll(/(\n|\t)/g, '') === ''
     || Array.from(Filtered.querySelectorAll('img[src*="//i.namu.wiki/i/"]')).length > 2
-    || Array.from(Filtered.querySelectorAll('span')).filter(Ele => getComputedStyle(Ele).getPropertyValue('background-image').startsWith('url(data:image/png;base64,')))
-    && Number(getComputedStyle(Filtered).getPropertyValue('padding-top').replaceAll('px', '')) >= 8 &&
+    || Array.from(Filtered.querySelectorAll('span')).filter(Ele => getComputedStyle(Ele).getPropertyValue('background-image').startsWith('url(data:image/png;base64,'))) &&
     Number(getComputedStyle(Filtered).getPropertyValue('height').replaceAll('px', '')) < 400 &&
-    getComputedStyle(Filtered).getPropertyValue('transition-timing-function') === 'ease-in' &&
-    Number(getComputedStyle(Filtered).getPropertyValue('transition-duration').replaceAll('s', '')) >= 0.025 &&
     Array.from(Filtered.querySelectorAll('*')).filter(Child => getComputedStyle(Child).getPropertyValue('animation-iteration-count') === 'infinite').length > 8
   ).forEach(Target => Target.setAttribute('style', 'display: none !important;'))
 }, 2500)
