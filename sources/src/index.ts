ba5a830dbb90bcb11ff91e1113dfe2b879f44346
piquark6046 <j.w.so@adguard.com>
2025-03-29 09:49:17 +0000

    fix: add keyword

--- sources/src/index.ts
+++ sources/src/index.ts
@@ -8,7 +8,7 @@ Win.Function.prototype.apply = new Proxy(Win.Function.prototype.apply, {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
   apply(Target: typeof Function.prototype.apply, ThisArg: Function, Args: Parameters<typeof Function.prototype.apply>) {
     if (typeof ThisArg === 'function' && /switch *\(.+, *new/.test(ThisArg.toString()) && /for *\( *; *; *\)/.test(ThisArg.toString()) &&
-    ['encodeURIComponent', 'namu.wiki/w/', 'x-namuwiki-key', 'X-Chika', 'x-ruby', 'X-You', 'Uint8Array'].filter(Index => ThisArg.toString().includes(Index)).length > 3) {
+    ['encodeURIComponent', 'namu.wiki/w/', 'x-namuwiki-key', 'X-Chika', 'setTimeout', 'x-ruby', 'X-You', 'Uint8Array', 'referrer', 'throw new Error'].filter(Index => ThisArg.toString().includes(Index)).length > 3) {
       return
     }
     return Reflect.apply(Target, ThisArg, Args)
