81da5a65cb2c1d2ba32a949eb7a8d2369555b800
piquark6046 <j.w.so@adguard.com>
2025-03-29 01:20:55 +0000

    fix: handling mobile variant

--- sources/src/index.ts
+++ sources/src/index.ts
@@ -7,8 +7,8 @@ const Win = typeof unsafeWindow !== 'undefined' ? unsafeWindow : window
 Win.Function.prototype.apply = new Proxy(Win.Function.prototype.apply, {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
   apply(Target: typeof Function.prototype.apply, ThisArg: Function, Args: Parameters<typeof Function.prototype.apply>) {
-    if (typeof ThisArg === 'function' && /switch *\(.+, *new/.test(ThisArg.toString()) &&
-    (ThisArg.toString().includes('encodeURIComponent') || ThisArg.toString().includes('namu.wiki/w/') || ThisArg.toString().includes('x-namuwiki-key'))) {
+    if (typeof ThisArg === 'function' && /switch *\(.+, *new/.test(ThisArg.toString()) && /for *\( *; *; *\)/.test(ThisArg.toString()) &&
+    ['encodeURIComponent', 'namu.wiki/w/', 'x-namuwiki-key', 'X-Chika', 'x-ruby', 'X-You', 'Uint8Array'].filter(Index => ThisArg.toString().includes(Index)).length > 3) {
       return
     }
     return Reflect.apply(Target, ThisArg, Args)
