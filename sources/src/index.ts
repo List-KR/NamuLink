5490d5f30c06c7af5653c6f5a89b2e52ef6b5187
piquark6046 <j.w.so@adguard.com>
2025-03-30 12:52:22 +0000

    fix: add keywords and pattern

--- sources/src/index.ts
+++ sources/src/index.ts
@@ -7,11 +7,15 @@ const Win = typeof unsafeWindow !== 'undefined' ? unsafeWindow : window
 Win.Function.prototype.apply = new Proxy(Win.Function.prototype.apply, {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
   apply(Target: typeof Function.prototype.apply, ThisArg: Function, Args: Parameters<typeof Function.prototype.apply>) {
+    if (typeof ThisArg !== 'function') {
+      return Reflect.apply(Target, ThisArg, Args)
+    }
     let Stringified = ThisArg.toString()
-    if (typeof ThisArg === 'function' && /switch *\(.+, *new/.test(Stringified) && /for *\( *; *; *\)/.test(Stringified) && /; *break *; *case/.test(Stringified) && /\(\) *; *case/.test(Stringified) &&
-    ['Promise', 'X-Riko', 'split', 'headers', 'AbortController', 'includes', 'encodeURIComponent', 'namu.wiki/w/', 'x-namuwiki-key', 'X-Chika', 'setTimeout', 'x-ruby', 'X-You', 'Uint8Array', 'referrer', 'throw new Error'].filter(Index => Stringified.includes(Index)).length > 3) {
+    if (/for *\( *; *; *\) *switch *\( *[a-zA-Z0-9_\[\]\(\)]+ *= *[a-zA-Z0-9_\[\]\(\)]+ *{/.test(Stringified) && /; *break *; *case/.test(Stringified) && /; *case *[_a-zA-Z0-9\[\]\(\)+*-]+: *return/.test(Stringified)
+    && !(/throw *[a-zA-Z0-9_\[\]\(\)]+ *= *null *, *[a-zA-Z0-9_\[\]\(\)]+/.test(Stringified)) && !(/\( *this *, *arguments *\)/.test(Stringified)) && !(/new *_[a-xA-Z0-9]+\[('|")t[0-9]('|")\]/.test(Stringified))
+    && ['Promise', 'X-Riko', 'split', 'headers', 'AbortController', 'includes', 'encodeURIComponent', 'namu.wiki/w/', 'x-namuwiki-key', 'X-Chika', 'setTimeout', 'x-ruby', 'X-You', 'Uint8Array', 'referrer', 'xi'].filter(Index => Stringified.includes(Index)).length > 3) {
       console.debug('[NamuLink]: Function.prototype.apply:', ThisArg, Args)
-      return
+      return Reflect.apply(Target, () => {}, [])
     }
     return Reflect.apply(Target, ThisArg, Args)
   }
