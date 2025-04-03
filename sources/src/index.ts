b68fc37b91a2c84a920324cb1de5afcc11160f57
piquark6046 <j.w.so@adguard.com>
2025-04-03 09:27:12 +0000

    fix: use hiding instead of mocking a func

--- sources/src/index.ts
+++ sources/src/index.ts
@@ -4,38 +4,12 @@ declare const unsafeWindow: unsafeWindow
 
 const Win = typeof unsafeWindow !== 'undefined' ? unsafeWindow : window
 
-const OriginalFunctinPrototypeCall = Function.prototype.call
-const OriginalFunctionPrototypeToString = Function.prototype.toString
-
-let PositiveRegexps = [
-  /; *for *\( *; *; *\) *switch *\( *_[A-Za-z0-9]+\[/,
-  /; *case *[A-Za-z0-9 *+-]+: *_[A-Za-z0-9]+ *= *_[A-Za-z0-9]+/,
-  /&& *\(_[A-Za-z0-9]+ *= *_[A-Za-z0-9]+/,
-  /xi/
-]
-
 let HidePowerLink = () => {
-  Array.from(document.querySelectorAll('div[class*=" "] div[class]:not(:has(svg))')).filter(Filtered => Filtered instanceof HTMLElement &&
-    (Filtered.innerText.includes('파워링크') || Filtered.innerText.replaceAll(/(\n|\t)/g, '') === ''
-    || Array.from(Filtered.querySelectorAll('img[src*="//i.namu.wiki/i/"]')).length > 2
-    || Array.from(Filtered.querySelectorAll('span')).filter(Ele => getComputedStyle(Ele).getPropertyValue('background-image').startsWith('url(data:image/png;base64,'))) &&
-    Number(getComputedStyle(Filtered).getPropertyValue('height').replaceAll('px', '')) < 400 &&
-    Array.from(Filtered.querySelectorAll('*')).filter(Child => getComputedStyle(Child).getPropertyValue('animation-iteration-count') === 'infinite').length > 8
-  ).forEach(Target => Target.remove())
+  let Target = Array.from(document.querySelectorAll('div[class*=" "] div[class*=" "]:has(*[href="#s-1"]):has([data-filesize])'))
+  Target = Target.filter(El => El instanceof HTMLElement && Number(getComputedStyle(El).getPropertyValue('height').replaceAll('px', '')) < 300)
+  Target.forEach(El => {
+    El.remove()
+  })
 }
-
-Win.Function.prototype.call = new Proxy(Win.Function.prototype.call, {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
-  apply(Target: typeof Function.prototype.call, ThisArg: Function, Args: Parameters<typeof Function.prototype.call>) {
-    if (ThisArg === OriginalFunctinPrototypeCall) {
-      return Reflect.apply(Target, ThisArg, Args)
-    }
-    let Stringified = Reflect.apply(OriginalFunctionPrototypeToString, ThisArg, [])
-    if (Stringified.length < 5000 && PositiveRegexps.filter(Index => Index.test(Stringified)).length >= 4) {
-      HidePowerLink()
-    }
-    return Reflect.apply(Target, ThisArg, Args)
-  }
-})
 
 setInterval(HidePowerLink, 2500)
