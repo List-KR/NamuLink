9f706aadf36c882d38bf2f72c7dc6942ff4d5283
piquark6046 <j.w.so@adguard.com>
2025-04-03 03:00:24 +0000

    fix: mock Function.prototype.call instead

--- sources/src/index.ts
+++ sources/src/index.ts
@@ -4,29 +4,17 @@ declare const unsafeWindow: unsafeWindow
 
 const Win = typeof unsafeWindow !== 'undefined' ? unsafeWindow : window
 
-let PositiveRegExps = [
-  /('|")[0-9]+('|") *=== *\( *null *=== *\(_[A-Za-z0-9]+ *= *this/,
-  /\|\| *void *\([-A-Za-z0-9+* -]+ *\) *=== *_[A-Za-z0-9]+/,
-  /=== *_[A-Za-z0-9]+ *\? *void/,
-  /_[A-Za-z0-9]+ *= *[_A-Za-z0-9]+ *, *_[A-Za-z0-9]+ *= *[_A-Za-z0-9]+ *, *this *\[_[A-Za-z0-9]+\([A-Za-z0-9]+\)\] *= *new/,
-  /this\[_[A-Za-z0-9]+\([A-Za-z0-9]+\)\]\( *\( *\) *=> *\{ *(const|let|var)/,
-  /\) *\]\( *this *\) *; *} *\) *; *}/
+const OriginalFunctinPrototypeCall = Function.prototype.call
+const OriginalFunctionPrototypeToString = Function.prototype.toString
+
+let PositiveRegexps = [
+  /; *for *\( *; *; *\) *switch *\( *_[A-Za-z0-9]+\[/,
+  /; *case *[A-Za-z0-9 *+-]+: *_[A-Za-z0-9]+ *= *_[A-Za-z0-9]+/,
+  /&& *\(_[A-Za-z0-9]+ *= *_[A-Za-z0-9]+/,
+  /xi/
 ]
-let NegaitiveRegExps = [/(Promise|_[A-Za-z0-9]+)\( *_[A-Za-z0-9]+ *=> *(setTimeout|_[A-Za-z0-9]+)/]
 
-Win.Function.prototype.bind = new Proxy(Win.Function.prototype.bind, {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
-  apply(Target: typeof Function.prototype.bind, ThisArg: Function, Args: Parameters<typeof Function.prototype.bind>) {
-    let Stringified = ThisArg.toString()
-    if (PositiveRegExps.filter(Index => Index.test(Stringified)).length >= 3 && NegaitiveRegExps.every(Index => !Index.test(Stringified))) {
-      console.debug('[NamuLink]: Function.prototype.bind:', ThisArg, Args)
-      return Reflect.apply(Target, () => {}, [])
-    }
-    return Reflect.apply(Target, ThisArg, Args)
-  }
-})
-
-setInterval(() => {
+let HidePowerLink = () => {
   Array.from(document.querySelectorAll('div[class*=" "] div[class]:not(:has(svg))')).filter(Filtered => Filtered instanceof HTMLElement &&
     (Filtered.innerText.includes('파워링크') || Filtered.innerText.replaceAll(/(\n|\t)/g, '') === ''
     || Array.from(Filtered.querySelectorAll('img[src*="//i.namu.wiki/i/"]')).length > 2
@@ -34,4 +22,20 @@ setInterval(() => {
     Number(getComputedStyle(Filtered).getPropertyValue('height').replaceAll('px', '')) < 400 &&
     Array.from(Filtered.querySelectorAll('*')).filter(Child => getComputedStyle(Child).getPropertyValue('animation-iteration-count') === 'infinite').length > 8
   ).forEach(Target => Target.remove())
-}, 2500)
+}
+
+Win.Function.prototype.call = new Proxy(Win.Function.prototype.call, {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
+  apply(Target: typeof Function.prototype.call, ThisArg: Function, Args: Parameters<typeof Function.prototype.call>) {
+    if (ThisArg === OriginalFunctinPrototypeCall) {
+      return Reflect.apply(Target, ThisArg, Args)
+    }
+    let Stringified = Reflect.apply(OriginalFunctionPrototypeToString, ThisArg, [])
+    if (Stringified.length < 5000 && PositiveRegexps.filter(Index => Index.test(Stringified)).length >= 4) {
+      HidePowerLink()
+    }
+    return Reflect.apply(Target, ThisArg, Args)
+  }
+})
+
+setInterval(HidePowerLink, 2500)
